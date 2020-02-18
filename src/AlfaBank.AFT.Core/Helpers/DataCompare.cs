1b1efb687a81a28066791c7a8a45acaa5364b3a6
Fossegrim <egorsh@inbox.ru>
2020-02-18 21:22:37 +0300

    изменение data compare

--- src/AlfaBank.AFT.Core/Helpers/DataCompare.cs
+++ src/AlfaBank.AFT.Core/Helpers/DataCompare.cs
@@ -1,25 +1,34 @@
-﻿using System.Data;
+﻿using System.Collections.Generic;
+using System.Data;
 
 namespace AlfaBank.AFT.Core.Helpers
 {
     public static class DataCompare
     {
-        public static bool AreTablesTheSame(this DataTable table1, DataTable table2)
+        public static (bool, string) AreTablesTheSame(this DataTable table1, DataTable table2)
         {
-
+            var isEqual = true;
+            var errors = new List<string>();
             if (table1.Rows.Count != table2.Rows.Count || table1.Columns.Count != table2.Columns.Count)
-                return false;
-
-
-            for (int i = 0; i < table1.Rows.Count; i++)
             {
-                for (int c = 0; c < table1.Columns.Count; c++)
+                isEqual = false;
+                errors.Add($"Размеры первой таблицы ({table1.Rows.Count};{table1.Columns.Count}) " +
+                        $"не совпадают с размерами второй таблицы ({table2.Rows.Count};{table2.Columns.Count})");
+            }
+            if (isEqual)
+            {
+                for (int i = 0; i < table1.Rows.Count; i++)
                 {
-                    if (!Equals(table1.Rows[i][c], table2.Rows[i][c]))
-                        return false;
+                    for (int c = 0; c < table1.Columns.Count; c++)
+                    {
+                        if (!Equals(table1.Rows[i][c], table2.Rows[i][c]))
+                        {
+                            errors.Add($"Элементы таблиц в позиции ({i};{c}) не совпадают => \"{table1.Rows[i][c]}\" не равен \"{table2.Rows[i][c]}\"");
+                        }
+                    }
                 }
             }
-            return true;
+            return errors.Count > 0 ? (false, string.Join(". /n ", errors)) : (true, null);
         }
     }
 }
