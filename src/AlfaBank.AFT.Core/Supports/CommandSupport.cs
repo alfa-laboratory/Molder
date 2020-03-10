b5115e806321ab1fdad341e831946eeb5a23d245
Fossegrim <egorsh@inbox.ru>
2020-03-10 18:58:33 +0300

    добавление поддержки ожидания выполнения действий

--- src/AlfaBank.AFT.Core/Supports/CommandSupport.cs
+++ src/AlfaBank.AFT.Core/Supports/CommandSupport.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using AlfaBank.AFT.Core.Models.Web;
 using OpenQA.Selenium;
 
 namespace AlfaBank.AFT.Core.Supports
@@ -6,6 +8,14 @@ namespace AlfaBank.AFT.Core.Supports
     public class CommandSupport
     {
         private const int retry = 3;
+        private readonly TimeSpan _interval = TimeSpan.FromMilliseconds(25);
+
+        protected Driver _driverSupport;
+
+        public CommandSupport(Driver driverSupport)
+        {
+            this._driverSupport = driverSupport;
+        }
 
         public void SendCommand(Action webSupport)
         {
@@ -62,6 +72,25 @@ namespace AlfaBank.AFT.Core.Supports
                 {
                     attempts++;
                 }
+            }
+            return null;
+        }
+
+        public object WaitCommand<TResult>(Func<TResult> command)
+        {
+            return waitCommand(command);
+        }
+
+        private object waitCommand<TResult>(Func<TResult> command)
+        {
+            var stopwatch = new Stopwatch();
+
+            while (stopwatch.ElapsedMilliseconds <= _driverSupport.Timeout)
+            {
+                var objResult = command();
+                if (objResult != null)
+                    return objResult;
+                System.Threading.Thread.Sleep(_interval);
             }
             return null;
         }
