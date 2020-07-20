d8e03b6150b2ef11ca69560e02f0506078c9a50d
Fossegrim <egorsh@inbox.ru>
2020-07-20 13:00:43 +0300

    commit

--- src/EvidentInstruction.ReportPortal/Infrastructures/Reports/ReportType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AlfaBank.AFT.Core.Infrastructures.Reports
-{
-    public enum ReportType
-    {
-        None = 0,
-        Allure = 1,
-        ReportPortal = 2
-    }
-}
--- src/EvidentInstruction/Helpers/Log.cs
+++ src/EvidentInstruction/Helpers/Log.cs
@@ -1,8 +1,10 @@
 ﻿using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
+using System.Diagnostics.CodeAnalysis;
 
 namespace EvidentInstruction.Helpers
 {
+    [ExcludeFromCodeCoverage]
     public static class Log
     {
         public static ILogger Logger = new LoggerConfiguration()
