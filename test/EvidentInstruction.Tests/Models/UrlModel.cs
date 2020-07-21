8b5000f750b88d4440a20d3ef3febb0715d10ed1
Fossegrim <egorsh@inbox.ru>
2020-07-21 16:33:27 +0300

    ExcludeFromCodeCoverage for TestModel

--- test/EvidentInstruction.Tests/Models/UrlModel.cs
+++ test/EvidentInstruction.Tests/Models/UrlModel.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace EvidentInstruction.Tests.Models
 {
+    [ExcludeFromCodeCoverage]
     public class UrlModel
     {
         [Required(ErrorMessage = "Url is required")]
