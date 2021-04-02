5247dc4d877fe047e476ad05f657582fb89d3e63
Fossegrim <egorsh@inbox.ru>
2021-04-02 20:18:05 +0300

    add Molder.ReportPortal

---
+++ src/Molder.ReportPortal/ReportPortalLog.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using ReportPortal.Serilog;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+using System.Diagnostics.CodeAnalysis;
+using TechTalk.SpecFlow;
+
+namespace Molder.ReportPortal
+{
+    [ExcludeFromCodeCoverage]
+    [Binding]
+    public static class ReportPortalLog
+    {
+        [BeforeTestRun(Order = -100000000)]
+        public static void BeforeForReportPortal()
+        {
+            Helpers.Log.LoggerFactory.ConfigureLogger();
+        }
+
+        private static void ConfigureLogger(this ILoggerFactory factory)
+        {
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+                .WriteTo.ReportPortal()
+            .CreateLogger();
+
+            factory.AddSerilog(logger);
+        }
+    }
+}
