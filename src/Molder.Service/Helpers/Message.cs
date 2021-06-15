fd1bf2d50e4fda12339de614733921097914cfb6
dshurkov <zenden231@gmail.com>
2021-06-15 16:15:19 +0300

    Использовал внутренние методы проверки валидности xml/конвертации xml в строку в Molder.Service

--- src/Molder.Service/Helpers/Message.cs
+++ src/Molder.Service/Helpers/Message.cs
@@ -15,8 +15,8 @@ namespace Molder.Service.Helpers
                     return $"Request: {request.Url} {Environment.NewLine} with method {request.Method}";
                 default:
                     var requestContentAsString = (request.Content as StringContent).ReadAsStringAsync().GetAwaiter().GetResult();
-                    return requestContentAsString.TryParseToXml() ?
-                        $"Request: {request.Url} {Environment.NewLine} with method {request.Method} {Environment.NewLine} and content: {Environment.NewLine} {System.Security.SecurityElement.Escape(Reflection.ConvertObject<string>(requestContentAsString.ToXml()))}" :
+                    return Molder.Helpers.Validate.TryParseToXml(requestContentAsString) ?
+                        $"Request: {request.Url} {Environment.NewLine} with method {request.Method} {Environment.NewLine} and content: {Environment.NewLine} {Converter.CreateXMLEscapedString(requestContentAsString.ToXml())}" :
                         $"Request: {request.Url} {Environment.NewLine} with method {request.Method} {Environment.NewLine} and content: {Environment.NewLine} {requestContentAsString}";
             }
         }
@@ -28,8 +28,8 @@ namespace Molder.Service.Helpers
                 case null:
                     return $"Responce: {responce.Request.Url} status: {responce.StatusCode}";
                 default:
-                    return responce.Content.TryParseToXml() ?
-                        $"Responce: {responce.Request.Url} status: {responce.StatusCode} and content: {Environment.NewLine} {System.Security.SecurityElement.Escape(Reflection.ConvertObject<string>(responce.Content.ToXml()))}" :
+                    return Molder.Helpers.Validate.TryParseToXml(responce.Content) ?
+                        $"Responce: {responce.Request.Url} status: {responce.StatusCode} and content: {Environment.NewLine} {Converter.CreateXMLEscapedString(responce.Content.ToXml())}" :
                         $"Responce: {responce.Request.Url} status: {responce.StatusCode} and content: {Environment.NewLine} {responce.Content}"; ;
             }
         }
