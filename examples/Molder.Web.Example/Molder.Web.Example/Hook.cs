8aea77bd3308b481a263db72a392d566f472532d
FosseGrim <egorsh@inbox.ru>
2021-07-07 14:27:46 +0300

    Update Hook.cs

--- examples/Molder.Web.Example/Molder.Web.Example/Hook.cs
+++ examples/Molder.Web.Example/Molder.Web.Example/Hook.cs
@@ -5,14 +5,10 @@ namespace Molder.Web.Example
     [Binding]
     public static class Hook
     {
-#if DEBUG
-        /*
         [BeforeTestRun(Order = Int32.MinValue)]
         public static void PageObjectInit()
         {
             HealthCheck.IsHealthCheck();
         }
-        */
-#endif 
     }
 }
