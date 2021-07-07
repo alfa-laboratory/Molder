dc35b4cb8f8474e5e6c374af3b6dec40ef1c7583
egorsh0 <egorsh@inbox.ru>
2021-07-07 13:28:29 +0300

    if Debug для PageObjectInit

--- examples/Molder.Web.Example/Molder.Web.Example/Hook.cs
+++ examples/Molder.Web.Example/Molder.Web.Example/Hook.cs
@@ -7,10 +7,12 @@ namespace Molder.Web.Example
     [Binding]
     public static class Hook
     {
+#if DEBUG
         [BeforeTestRun(Order = Int32.MinValue)]
         public static void PageObjectInit()
         {
             HealthCheck.IsHealthCheck();
         }
+#endif 
     }
 }
