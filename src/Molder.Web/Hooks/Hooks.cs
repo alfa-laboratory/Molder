523ce4e3aa78a78460b5cd00263cb21dbe4dd9b2
EduardSosnerzh <edi.mow@gmail.com>
2021-07-29 12:19:10 +0300

    Added Log information about PageObject tree

--- src/Molder.Web/Hooks/Hooks.cs
+++ src/Molder.Web/Hooks/Hooks.cs
@@ -7,6 +7,7 @@ using Molder.Web.Helpers;
 using Molder.Web.Infrastructures;
 using Molder.Web.Models;
 using Molder.Web.Models.Settings;
+using System.Collections.Generic;
 using TechTalk.SpecFlow;
 
 namespace Molder.Web.Hooks
@@ -37,6 +38,26 @@ namespace Molder.Web.Hooks
 
             TreePages.SetVariables(variableController);
             TreePages.Get();
+            var _var = TreePages.Get();
+            LogPageObject(_var);
+        }
+
+        public static void LogPageObject(IEnumerable<Node> pages, int level = 0)
+        {
+            string s = "";
+            for (int i = 0; i < level; i++)
+            {
+                s += "|   ";
+            }
+            foreach (Node page in pages)
+            {
+                if (!(page.Childrens is null))
+                {
+                    Log.Logger().LogDebug(s + "└───" + page.Type.ToString() + "(" + page.Name + ")");
+                    LogPageObject(page.Childrens, level + 1);
+                }
+                else Log.Logger().LogDebug(s + "|   " + page.Type.ToString() + "(" + page.Name + ")");
+            }
         }
 
         [AfterScenario()]
