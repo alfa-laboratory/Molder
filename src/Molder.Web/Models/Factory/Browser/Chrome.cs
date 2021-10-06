aa249d124a12b49a530c2c9f320c4923a4bfb2f7
egorsh0 <ironman@789>
2021-10-06 13:35:08 +0300

    Добавлен commandTimeout для Chrome

--- src/Molder.Web/Models/Factory/Browser/Chrome.cs
+++ src/Molder.Web/Models/Factory/Browser/Chrome.cs
@@ -16,10 +16,11 @@ namespace Molder.Web.Models.Browser
         public Chrome()
         {
             var options = CreateOptions();
+            var timeout = BrowserSettings.Settings.Timeout ?? Constants.DEFAULT_TIMEOUT;
             if(BrowserSettings.Settings.IsRemoteRun())
             {
                 Log.Logger().LogInformation($@"Start remote chrome browser...");
-                DriverProvider.CreateDriver(() => new RemoteWebDriver(new Uri(BrowserSettings.Settings.Remote.Url), options.ToCapabilities()));
+                DriverProvider.CreateDriver(() => new RemoteWebDriver(new Uri(BrowserSettings.Settings.Remote.Url), options.ToCapabilities(), TimeSpan.FromSeconds(timeout)));
                 SessionId = (DriverProvider.GetDriver() as RemoteWebDriver)?.SessionId;
                 Log.Logger().LogInformation($@"Remote chrome browser (SessionId is {SessionId}) is starting with options: {Helpers.Message.CreateMessage(options)}");
                 return;
@@ -27,7 +28,7 @@ namespace Molder.Web.Models.Browser
             Log.Logger().LogInformation($@"Start chrome browser...");
             var service = ChromeDriverService.CreateDefaultService();
             service.HideCommandPromptWindow = true;
-            DriverProvider.CreateDriver(() => new ChromeDriver(service, options));
+            DriverProvider.CreateDriver(() => new ChromeDriver(service, options, TimeSpan.FromSeconds(timeout)));
             SessionId = (DriverProvider.GetDriver() as ChromeDriver)?.SessionId;
             Log.Logger().LogInformation($@"Local chrome browser (SessionId is {SessionId}) is starting with options: {Helpers.Message.CreateMessage(options)}");
         }
